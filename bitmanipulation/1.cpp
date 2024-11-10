271590d00d158126cdaacae613db646388c9098e
Your Name <you@example.com>
2024-11-10 20:56:44 +0530

    hi

---
+++ bitmanipulation/1.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+int size;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define rep(i, a, b) for (int i = a; i <= b; i++)
+#define repr(i, a, b) for (int i = a; i >= b; i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)((x).size())
+#define pb push_back
+#define F first
+#define S second
+#define endl '\n'
+
+template<typename T>
+void print_vector(const vector<T>& v) {
+    for (const auto& elem : v) cout << elem << ' ';
+    cout << '\n';
+}
+
+template<typename T>
+void input_vector(vector<T>& v) {
+    for (auto& elem : v) cin >> elem;
+}
+
+int main() {
+    fast_io;
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int x;
+        cin >> x;
+        // Your code here
+    }
+
+    return 0;
+}
